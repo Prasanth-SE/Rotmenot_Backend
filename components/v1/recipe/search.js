6f6bc8ee0264c795f1146b14133aad2365a23640
JM947716 <jmallipeddi@albany.edu>
2021-11-03 17:41:32 -0400

    added ingredient quantity to recipe

--- components/v1/recipe/search.js
+++ components/v1/recipe/search.js
@@ -12,18 +12,10 @@
          let where = {};
          where.status = "published";
          if (body.name){
-             where.name = new RegExp(body.name);
-         }
-         const test = ['6166cd1358185a06c78d07fa', '61624af5acf5334370f20ab8'];
-         if (body.ingredients){
-             where.$and = [
-                 { "ingredients": { "$all": body.ingredients } },
-                 { "ingredients": { "$size": body.ingredients.length } }
-             ];
-         }
-         console.log(where)
-         const recipes = await db.Recipe.find(where).sort({ "updatedAt": -1 });
-
+            where.name = new RegExp(body.name);
+        }
+        console.log(where)
+        const recipes = await db.Recipe.find(where).sort({ "updatedAt": -1 });
          return res.success({
              recipes
          });
